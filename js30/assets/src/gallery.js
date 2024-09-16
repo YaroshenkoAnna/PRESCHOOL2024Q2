8ebf6f440fbd7fc20cc563c4e225b92f870b342c
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2024-09-16 17:06:37 +0200

    feat: add main functional

--- js30/assets/src/gallery.js
+++ js30/assets/src/gallery.js
@@ -1,19 +1,53 @@
-const url = "https://api.unsplash.com/photos/?client_id=xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo";
 const gallery = document.querySelector(".gallery");
-const keyAPI = "xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo"
-const searchUrl = "https://api.unsplash.com/search/photos?page=3&per_page=30&query=office&orientation=landscape&client_id=xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo" 
+const search = document.querySelector(".search__field");
+const searchButton = document.querySelector(".search__button");
 
-async function getData() {
-  const res = await fetch(searchUrl);
+const keyAPI = "xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo"
+let searchUrl = `https://api.unsplash.com/search/photos?page=3&per_page=30&query="nature"
+}&orientation=landscape&color=black_and_white&client_id=xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo`;
+const colorThema = ["black_and_white", "black", "white", "yellow", "orange", "red", "purple", "magenta", "green", "teal", "blue"];
+const languages = ["en", "ru", "uk", "de"];
+
+search.addEventListener("keydown", isEnter);
+searchButton.addEventListener ("click", createURL);
+
+function isEnter(event){
+    if (event.code === "Enter") {
+        createURL();    
+    }
+}
+
+function createURL(lang){
+    let searchValue = search.value; 
+    let i =  typeof lang === "number" ? lang : 0;
+     searchUrl = `https://api.unsplash.com/search/photos?page=3&per_page=30&query=${searchValue
+}&orientation=landscape&color=black_and_white&lang=${languages[i]}&client_id=xnPITw_67ECTxQDMG6GElKHBcJB1fDz2lE39zuw9Neo`;
+        getData(searchUrl, i);    
+}
+     
+   
+
+
+async function getData(url, index) {
+  const res = await fetch(url);
   const data = await res.json();
   console.log(data);
+  if (data.results.length === 0) {
+      index++;
+      index < languages.length ? createURL(index) : "";
+  }
   createImages(data.results);
   
 }
 
-getData();
+getData(searchUrl);
 
 function createImages(images){
+    if (images.length > 0) {
+        gallery.replaceChildren();
+    }else{
+
+    }
       for (let i = 0; i < images.length; i++) {
           const imageLink = images[i].urls.small;
           const altText = images[i].alt_description;
